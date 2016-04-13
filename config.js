adfd6ccdb38f23817722d45d0aec071bb4090651
John Billingsley <jmb330@case.edu>
2016-04-12 18:03:40 -0700

    created duck repo

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'DmKkrFCpjvHLBs5ZoOma6WMfu',
+  consumer_secret:      'dN7LaOSiQrRGq3MzgovneYg9Ec2Ng9NjtKFh6Ql8k3eW3xF5eE',
+  access_token:         '717185304960970754-z23pOpCRdNOn31govcAbxjKD3r8U4EX',
+  access_token_secret:  'ebQgU4ZLWSu0mpUymqqUJL7gjAbstECp3Byhx6NYVdYvW'
+}
